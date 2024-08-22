0e8dacfd4ea150eec440cdc746d2bcbf4f3bca16
Rex Peters <rexpeters@Rexs-MacBook-Pro.local>
2024-08-21 17:19:19 -0700

    Setup initial folders and main.py documents with initial code to read proper csv files

---
+++ PyBank/main.py
@@ -0,0 +1,9 @@
+import csv
+# Path to the CSV 
+csvpath = 'Resources/budget_data.csv'
+# Open and read the CSV file
+with open(csvpath, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    # Skip the header
+    next(csvreader)
+
